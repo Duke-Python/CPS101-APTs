b103d38d0fe4ee502eb045dd3fb697ded64368a1
George S. Hugh <hughgs@duke.edu>
2022-10-03 18:17:33 -0400

    Fix DNACodingRegion.py to test workflow.

--- DNACodingRegion.py
+++ DNACodingRegion.py
@@ -10,14 +10,14 @@ def findRegion(dna):
     print("=============================")
     print(dna)
     retval = ""
-    startCodon = "atg"
-    stopCodons = ['taa', 'tag', 'tga']
-    val1 = dna.find(startCodon)
-    if startCodon not in dna:
+    start_codon = "atg"
+    stop_codons = ['taa', 'tag', 'tga']
+    val1 = dna.find(start_codon)
+    if start_codon not in dna:
         return retval
 
     for i in range(len(dna)):
-        if dna[val1+i:val1+i+3] in stopCodons:
+        if dna[val1+i:val1+i+3] in stop_codons:
             if (len(dna[val1:val1+i+3]) % 3) == 0:
                 retval += dna[val1+3:val1+i]
                 return retval
